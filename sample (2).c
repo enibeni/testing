5e89299ef11405730acf82b239b24f0f260febab
Sergey <enibeni@ya.ru>
2016-03-05 17:09:57 +0300

    add ";"

--- sample (2).c
+++ sample (2).c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-printf("hello world");
-printf("hello enibeni");
-printf("hello katik");
+printf("hello world;");
+printf("hello enibeni;");
+printf("hello katik;");
 return 0;
 }
